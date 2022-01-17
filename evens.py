d1e8f1a13268b785b7416b942285517530e3062b
mjjstockman <mjjstockman@gmail.com>
2022-01-17 12:09:18 +0000

    Add test_evens function in TDD manner

--- evens.py
+++ evens.py
@@ -6,7 +6,22 @@ def even_number_of_evens(numbers):
     if the number of even numbers is odd - return False
     if the numner of even numbers is even - return True
     """
-    return True
+    if isinstance(numbers, list):
+        if numbers == []:
+            return False
+        else:
+            evens = 0
+
+            for n in numbers:
+                if n % 2 == 0:
+                    evens += 1
+
+            if evens:
+                return evens % 2 == 0
+            else:
+                return False
+    else:
+        raise TypeError("A list was not passed into the function")
 
 # only run if file is named __main__ (). Python names any file it runs directly
 # as __main__. therefore below code will only run when this file is run, and
--- test_evens.py
+++ test_evens.py
@@ -6,11 +6,20 @@ from evens import even_number_of_evens
 
 # a test case which inherits from unittest.TestCase class
 class TestEvens(unittest.TestCase):
+
+    def test_throws_error_if_value_passed_is_not_list(self):
+        self.assertRaises(TypeError, even_number_of_evens, 4)
     # # method name must begin with test_...
     # def test_function_returns_True(self):
     #     # use self to mean the current instance of the class
     #     self.assertTrue(even_number_of_evens([]))
-    pass
+    
+    def test_values_in_list(self):
+        self.assertEqual(even_number_of_evens([]), False)
+        self.assertEqual(even_number_of_evens([2, 4]), True)
+        self.assertEqual(even_number_of_evens([2]), False)
+        self.assertEqual(even_number_of_evens([1, 3, 5]), False)
+
 
 
 if __name__ == "__main__":
